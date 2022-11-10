c69b7e36712c44f2b4506704f77932aec59e3be9
Yusuf K. Muhammad <kyusufm@gmail.com>
2022-11-10 23:14:58 +0700

    Update App.js
    Import components.
    
    import styles.
    
    set basic blueprint to display pokemon data.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Main from './components/Main';
+import Header from './components/Header';
+import ComparePane from './components/ComparePane';
+import PokeInfo from './components/PokeInfo';
+
+import './styles/reset.css';
+import './styles/App.css';
+import './styles/header.css';
+import './styles/pokebox.css';
+import './styles/style.css';
+import './styles/compare.css';
+import './styles/button.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div id="root">
+            <Header/>
+            <main className="page">
+                <h1 className="page-title">Pokédex <span>(905)</span></h1>
+
+                <Main/>
+
+                <ComparePane/>
+            </main>
+            {/* <PokeInfo/> */}
+        </div>
+    )
 }
 
 export default App;
